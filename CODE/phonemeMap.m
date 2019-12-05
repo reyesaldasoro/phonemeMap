5fc2b60e2b376116f91edf873fb09de2bd66f842
CCRA <ccr22@lcladms-Mac-Pro.local>
2019-12-05 14:49:48 +0000

    Update phonemeMap.m
    arrange names and plotting the circles, better with plot than viscircle

--- CODE/phonemeMap.m
+++ CODE/phonemeMap.m
@@ -1,6 +1,13 @@
 
 
 
+h0          = figure(1);
+h0.Position = [    891   361   939   612];
+h1          = gca;
+h1.Position = [0.0601    0.0642    0.8542    0.8782];
+
+
+
 %% Position the phonemes, currently based on 
 % https://www.englishclub.com/pronunciation/phonemic-chart.htm
 % a 6 x 8 location with 3 spaces separation
@@ -20,23 +27,37 @@ yy                      = y(:);
 numberPhonemes          = numel(xx);
 radii                   = ones(numberPhonemes,1);
 names                   = num2str((1:numberPhonemes)');
-%weights                 = 3*rand(48,1);
+weights                 = 3*rand(48,1);
+% colour allocation
+% circ_colour             = rand(48,3);
+circ_colour             = zeros(48,3);
+circ_colour([1:4  9:12 17:20],1)    = 0.7;
+circ_colour([5:8 13:16 21:24],2)    = 0.4;
+circ_colour([25:48],3)              = 0.6;
+
+
 cla
+hold on
 minCircleSize           = 0.7;
 weights(weights<minCircleSize)    = minCircleSize;
+weights([7 8 16]) = 0.0001;
 textSize                = 20*ones(numberPhonemes,1);
-viscircles([xx(:) yy(:)],weights);
+%hCircles                = viscircles([xx(:) yy(:)],weights,'color',0.7*[1 1 1]);
 
-hText = text(xx(:)-0.2*weights, yy(:),names2,'fontsize',24);
+%hText = text(xx(:)-0.4*weights, yy(:),names2,'fontsize',24);
 for k=1:numberPhonemes
-    hText(k).FontSize = textSize(k)*weights(k);
+    hCircles(k)         = plot(xx(k),yy(k),'o','markersize',40*(weights(k)),'color',circ_colour(k,:),'markerfacecolor',circ_colour(k,:),'linewidth',0.5);
+    hText(k)            = text(xx(k)-0.4*weights(k),yy(k),names2{k},'fontsize',23);
+    hText(k).FontSize   = textSize(k)*weights(k);
+    hText(k).Color      = 'w';%circ_colour(k,:); % colour allocation
+    %hCircles(k)         = viscircles([xx(k) yy(k)],weights(k),'color',circ_colour(k,:),'linewidth',weights(k));
 end
 grid on;axis tight
 axis equal
 axis off
 axis ij
 %%
-names2 = {'?','i:','?','u:','e','?:','?','?:','æ','?','?','?:','e?','??','a?','e?','??','??','??','a?','/','p' ,'f','?','t','s','?','?','k','b','v'	'ð','d','z','?','?','g','m','n','?','h','w','l','r','j','','',''};
+names2 = {'?','i:','?','u:','??','e?','','','e','?','?:','?:','??','??','??','','æ','?','?:','?','e?','a?','a?','/','p','b','t','d','?','?','k','g','f','v','?','ð','s','z','?','?','m','n','?','h','l','r','w','j'};
 %ship,sheep,book,shoot
 % left,her,teacher,door
 % hat,up,on,far
